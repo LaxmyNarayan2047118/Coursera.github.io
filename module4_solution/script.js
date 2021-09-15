81c1f04169c0e404552a12cc1ce8e2b0557bea8c
laxmy Narayan <69455713+LaxmyNarine@users.noreply.github.com>
2021-09-15 20:15:51 +0530

    Update script.js

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -1,22 +1,23 @@
-/*
-Solution of assignment 4:
 
-Expected output:
-
-Hello laxmy
-Good Bye raj
-Good Bye hari
-Good Bye harish
-Hello san
-Hello sui
-Hello sona
-Hello angel
-Hello akan
-Good Bye shiva
-*/
 
 (function() {
-    var names = ["laxmy", "raj", "hari", "harish", "san", "sui", "sona", "angel", "akan", "shiva"];
+    var names = [" Yaakov",
+" John",
+ "Jen",
+
+"Jason",
+
+"Paul",
+
+"Frank",
+
+"Larry",
+
+"Paula",
+
+"Laura",
+
+ "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
